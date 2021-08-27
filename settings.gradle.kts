352c2986bd904fa792839b8951d6a00506df75f6
HalalStuff <darthsion626@gmail.com>
2021-08-26 21:27:10 -0500

    empty

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+include(":HalalPlugins")
+
+include(":NoReactions")
+project(":NoReactions").projectDir = File("./NoReactions")
+
+include(":VCID")
+project(":VCID").projectDir = File("./VCID")
+
+rootProject.name = "HalalPlugins"
