7c488c11b0ec4299ee6c9c0277931300117dfa28
HalalStuff <darthsion626@gmail.com>
2021-12-03 19:29:33 -0600

    Add shitty download stickers

--- ShittyDownloadStickers/src/main/java/com/aliucord/plugins/ShittyDownloadStickers.java
+++ ShittyDownloadStickers/src/main/java/com/aliucord/plugins/ShittyDownloadStickers.java
@@ -5,19 +5,23 @@ import android.os.Environment;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.aliucord.Http;
 import com.aliucord.Logger;
 import com.aliucord.Utils;
 import com.aliucord.annotations.AliucordPlugin;
 import com.aliucord.entities.Plugin;
-import com.aliucord.patcher.PinePatchFn;
+import com.aliucord.patcher.Hook;
+import com.aliucord.plugins.clonemodal.Modal;
 import com.aliucord.utils.DimenUtils;
 import com.aliucord.views.Button;
 import com.discord.api.sticker.BaseSticker;
+import com.discord.api.sticker.Sticker;
 import com.discord.databinding.WidgetGuildStickerSheetBinding;
-import com.discord.models.guild.Guild;
+import com.discord.widgets.stickers.GuildStickerSheetViewModel;
 import com.discord.widgets.stickers.WidgetGuildStickerSheet;
+import com.lytefast.flexinput.R;
 
 import java.io.File;
 
@@ -25,7 +29,7 @@ import java.io.File;
 @SuppressWarnings("unused")
 @AliucordPlugin
 public class ShittyDownloadStickers extends Plugin {
-    private final Logger log = new Logger();
+    public static final Logger log = new Logger("ShittyDownloadStickers");
     private File downloadDirectory = new File(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)));
     @Override
     // Called when your plugin is started. This is the place to register command, add patches, etc
@@ -33,43 +37,121 @@ public class ShittyDownloadStickers extends Plugin {
         final int layoutId = View.generateViewId();
         var method = WidgetGuildStickerSheet.class.getDeclaredMethod("getBinding");
         method.setAccessible(true);
-        patcher.patch(WidgetGuildStickerSheet.class, "configureButtons", new Class<?>[] { boolean.class, boolean.class, Guild.class}, new PinePatchFn(callFrame -> {
+        patcher.patch(WidgetGuildStickerSheet.class, "configureUI", new Class<?>[] { GuildStickerSheetViewModel.ViewState.Loaded.class }, new Hook(callFrame -> {
             try {
+                // i looked at ven's plugin for help since i have no idea what im doing
+                var sticker = ( (GuildStickerSheetViewModel.ViewState.Loaded) callFrame.args[0]).component1();
+                var nitro = ( (GuildStickerSheetViewModel.ViewState.Loaded) callFrame.args[0]).component2();
+                var isKnown = ( (GuildStickerSheetViewModel.ViewState.Loaded) callFrame.args[0]).getGuildStickerGuildInfo() != null;
+                var isUserInGuild = ( (GuildStickerSheetViewModel.ViewState.Loaded) callFrame.args[0]).getGuildStickerGuildInfo().isUserInGuild();
+
                 var binding = (WidgetGuildStickerSheetBinding) method.invoke(callFrame.thisObject);
-                var ctx = binding.getRoot().getContext();
-                FrameLayout layout = (FrameLayout) ((ViewGroup)((ViewGroup) binding.getRoot()).getChildAt(0)).getChildAt(1);
-                layout.setVisibility(View.VISIBLE);
+                if (binding == null) return;
+                var root = (ViewGroup) binding.getRoot();
+                var rootLayout = (android.widget.LinearLayout) root.getChildAt(0);
+
+                if (rootLayout.findViewById(layoutId) != null) return;
+
+                var ctx = rootLayout.getContext();
+                if (ctx == null) return;
+
+                int marginDpFour = DimenUtils.dpToPx(4);
+                int marginDpEight = marginDpFour * 2;
+                int marginDpSixteen = marginDpEight * 2;
 
                 var downloadButton = new Button(ctx);
-                downloadButton.setText("Download Sticker");
-                downloadButton.setPadding(DimenUtils.dpToPx(18), DimenUtils.dpToPx(18), DimenUtils.dpToPx(18), DimenUtils.dpToPx(18));
-                downloadButton.setOnClickListener( (view) -> {
-                    String url = getCDNAssetUrl(binding.m.j);
-                        new Thread(() -> {
-                            try {
-                                File stickerSave = new File(downloadDirectory, binding.m.j.d()+binding.m.j.b());
-                                Http.simpleDownload(url, stickerSave);
-                                Utils.showToast(context, String.format("Saved sticker to %s", stickerSave.getAbsolutePath()));
-                            } catch (Throwable e) { log.error(context, "Failed to download sticker."); }
-                        }).start();
-                });
-                /**
+                configureDownload(context, sticker, downloadButton);
+
                 var copyUrl = new Button(ctx);
                 copyUrl.setText("Copy URL");
                 copyUrl.setPadding(DimenUtils.dpToPx(18), DimenUtils.dpToPx(18), DimenUtils.dpToPx(18), DimenUtils.dpToPx(18));
                 copyUrl.setOnClickListener( (view) -> {
-                    String url = getCDNAssetUrl(binding.m.j);
+                    String url = getCDNAssetUrl(sticker);
                     Utils.setClipboard("URL", url);
+                    Utils.showToast("Copied to clipboard");
                 });
 
-                buttonLayout.addView(downloadButton);
-                buttonLayout.addView(copyUrl);
-                 */
-                layout.addView(downloadButton,2 );
+                var cloneButton = new Button(ctx);
+                cloneButton.setText("Clone to other server");
+                cloneButton.setOnClickListener(v -> Utils.openPageWithProxy(ctx, new Modal(getCDNAssetUrl(sticker), sticker.h(), sticker.getId(), sticker.f(), sticker.j())));
+
+                var buttonParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+                buttonParams.setMargins(0, 0, 0, 0);
+                downloadButton.setLayoutParams(buttonParams);
+                copyUrl.setLayoutParams(buttonParams);
+                cloneButton.setLayoutParams(buttonParams);
+
+                var pluginButtonLayout = new com.aliucord.widgets.LinearLayout(ctx);
+                pluginButtonLayout.setId(layoutId);
+                pluginButtonLayout.addView(downloadButton);
+                pluginButtonLayout.addView(copyUrl);
+                pluginButtonLayout.addView(cloneButton);
+
+                int idx = 2;
+                if (
+                        (!nitro /* need nitro */ ||
+                                !isUserInGuild /* not on server */
+                        ) && isKnown
+                ) {
+                    // Nitro or Join Button visible
+                    pluginButtonLayout.setPadding(marginDpSixteen, marginDpFour, marginDpSixteen, marginDpEight);
+
+                    // Adjust nitro and join button
+                    var params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+                    params.setMargins(0, 0, 0, 0);
+                    binding.k.setLayoutParams(params); // Nitro
+                    binding.l.setLayoutParams(params); // Join
+
+                    // Adjust nitro/join container
+                    var joinContainer = (FrameLayout) binding.k.getParent();
+                    var containerParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    containerParams.setMargins(marginDpSixteen, marginDpEight, marginDpSixteen, 0);
+                    joinContainer.setLayoutParams(containerParams);
+                } else {
+                    // No buttons
+                    pluginButtonLayout.setPadding(marginDpSixteen, marginDpEight, marginDpSixteen, marginDpEight);
+                }
+
+                rootLayout.addView(pluginButtonLayout, idx);
             } catch (Exception e) {
                 log.error(context, e);
             }
         }));
+    }
+
+    private void configureDownload(Context context, Sticker sticker, Button downloadButton) {
+        Runnable callSelf = () -> configureDownload(context, sticker, downloadButton);
+
+        File stickerSave = new File(downloadDirectory, sticker.d() + sticker.b());
+        downloadButton.setPadding(DimenUtils.dpToPx(18), DimenUtils.dpToPx(18), DimenUtils.dpToPx(18), DimenUtils.dpToPx(18));
+        if (!stickerSave.exists()) {
+            downloadButton.setText("Save Sticker");
+            downloadButton.setBackgroundColor(context.getColor(R.c.uikit_btn_bg_color_selector_brand));
+            downloadButton.setOnClickListener((view) -> {
+                Utils.threadPool.execute(() -> {
+                    try {
+                        String url = getCDNAssetUrl(sticker);
+
+                        Http.simpleDownload(url, stickerSave);
+                        Utils.showToast(String.format("Saved sticker to %s", stickerSave.getAbsolutePath()));
+                        callSelf.run();
+                    } catch (Throwable e) {
+                        log.error(context, "Failed to download sticker.");
+                    }
+                });
+            });
+        } else {
+            downloadButton.setText("Remove Saved Sticker");
+            downloadButton.setBackgroundColor(context.getColor(R.c.uikit_btn_bg_color_selector_red));
+            downloadButton.setOnClickListener((view) -> {
+                Utils.threadPool.execute(() -> {
+                    if (stickerSave.delete())
+                        callSelf.run();
+                    else
+                        Utils.showToast("Failed to delete sticker :(");
+                });
+            });
+        }
     }
 
     @Override
@@ -79,12 +161,7 @@ public class ShittyDownloadStickers extends Plugin {
     }
 
     private final String getCDNAssetUrl(BaseSticker baseSticker) {
-        int ordinal = baseSticker.a().ordinal();
-        if (ordinal == 1 || ordinal == 2) {
-            return "https://media.discordapp.net/stickers/" + baseSticker.d() + baseSticker.b();
-        } else {
-            return "https://discord.com/stickers/" + baseSticker.d() + baseSticker.b();
-        }
+        return "https://discord.com/stickers/" + baseSticker.d() + baseSticker.b();
     }
 
 }
---
+++ ShittyDownloadStickers/src/main/java/com/aliucord/plugins/clonemodal/Modal.java
@@ -0,0 +1,123 @@
+/*
+ * Ven's Aliucord Plugins
+ * Copyright (C) 2021 Vendicated
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+package com.aliucord.plugins.clonemodal;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.aliucord.CollectionUtils;
+import com.aliucord.Http;
+import com.aliucord.Utils;
+import com.aliucord.fragments.SettingsPage;
+import com.aliucord.plugins.ShittyDownloadStickers;
+import com.discord.api.permission.Permission;
+import com.discord.models.guild.Guild;
+import com.discord.stores.StoreGuilds;
+import com.discord.stores.StoreStream;
+import com.discord.utilities.permissions.PermissionUtils;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Modal extends SettingsPage {
+    private static final Map<Integer, Integer> stickerLimits = new HashMap<>();
+    static {
+        stickerLimits.put(0, 0);
+        stickerLimits.put(1, 15);
+        stickerLimits.put(2, 30);
+        stickerLimits.put(3, 60);
+    }
+
+    private final Map<Long, Long> guildPerms = StoreStream.getPermissions().getGuildPermissions();
+    private final StoreGuilds guildStore = StoreStream.getGuilds();
+
+    private final String name;
+    private final String url;
+    private final long id;
+    private final String description;
+    private final String tags;
+
+    public Modal(String url, String name, long id, String description, String tags) {
+        this.url = url;
+        this.name = name;
+        this.id = id;
+        this.description = description;
+        this.tags = tags;
+    }
+
+    @Override
+    public void onViewBound(View view) {
+        super.onViewBound(view);
+
+        setActionBarTitle("Clone Sticker");
+        setActionBarSubtitle(name);
+
+        var ctx = view.getContext();
+
+        setPadding(0);
+
+        var recycler = new RecyclerView(ctx);
+        recycler.setLayoutManager(new LinearLayoutManager(ctx, RecyclerView.VERTICAL, false));
+        var adapter = new Adapter(this, CollectionUtils.filter(guildStore.getGuilds().values(), this::isCandidate));
+        recycler.setAdapter(adapter);
+
+        addView(recycler);
+    }
+
+    private boolean isCandidate(Guild guild) {
+        var perms = guildPerms.get(guild.getId());
+        if (!PermissionUtils.can(Permission.MANAGE_EMOJIS_AND_STICKERS, perms)) return false;
+        int usedSlots = 0;
+        for (var sticker : guild.getStickers()) {
+            if (sticker.getId() == id) return false;
+
+            usedSlots++;
+        }
+        var slots = stickerLimits.get(guild.getPremiumTier());
+        return slots != null && usedSlots < slots;
+    }
+
+    public void clone(Context ctx, Guild guild) {
+        Utils.threadPool.execute(() -> {
+
+            try {
+                var request = Http.Request.newDiscordRequest("/guilds/"+guild.getId()+"/stickers", "POST");
+                var tempFile = File.createTempFile("sticker", ".png");
+                Http.simpleDownload(url, tempFile);
+
+                request.executeWithMultipartForm(Map.of("file", tempFile, "name", name, "description", description != null ? description : "", "tags", tags));
+            } catch (Exception e) {
+                ShittyDownloadStickers.log.error(ctx, e);
+            }
+
+            /**
+            var api = RestAPI.getApi();
+            var uri = imageToDataUri();
+            if (uri == null) {
+                //EmojiUtility.logger.error(ctx, "Something went wrong while preparing the image");
+                return;
+            }
+            var obs = api.postGuildEmoji(guild.getId(), new RestAPIParams.PostGuildEmoji(name, uri));
+            var res = RxUtils.getResultBlocking(obs);
+            /**if (res.second == null)
+                EmojiUtility.logger.info(ctx, String.format("Successfully cloned %s to %s", name, guild.getName()));
+            else
+                EmojiUtility.logger.error(ctx, "Something went wrong while cloning this emoji", res.second);*/
+
+
+        });
+    }
+}
