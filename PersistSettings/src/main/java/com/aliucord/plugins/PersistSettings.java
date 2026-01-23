af9480f72fad670506e0722173e517167758d1ce
Archimedes9500 <82618548+Archimedes9500@users.noreply.github.com>
2026-01-23 01:14:55 +0100

    Update PersistSettings.java
    Update to latest discord version

--- PersistSettings/src/main/java/com/aliucord/plugins/PersistSettings.java
+++ PersistSettings/src/main/java/com/aliucord/plugins/PersistSettings.java
@@ -19,8 +19,11 @@ import com.discord.stores.StoreNux;
 import com.discord.stores.StoreStickers;
 import com.discord.utilities.persister.Persister;
 import com.discord.views.CheckedSetting;
+import com.discord.models.authentication.AuthState;
+import com.discord.stores.authentication.AuthStateCache;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import d0.z.d.m;
 
@@ -58,46 +61,43 @@ public class PersistSettings extends Plugin {
     @Override
     // Called when your plugin is started. This is the place to register command, add patches, etc
     public void start(Context context) throws NoSuchMethodException, NoSuchFieldException {
-        Field authToken = StoreAuthentication.class.getDeclaredField("authToken");
-        authToken.setAccessible(true);
+        Field fAuthState = StoreAuthentication.class.getDeclaredField("authState");
+        fAuthState.setAccessible(true);
 
-        try {
-        patcher.patch(StoreAuthentication.class.getDeclaredMethod("handleAuthToken$app_productionCanaryRelease", String.class), new InsteadHook(callFrame -> {
-            return getObject(authToken, callFrame);
-        })); } catch (NoSuchMethodException e) {
-            patcher.patch(StoreAuthentication.class.getDeclaredMethod("handleAuthToken$app_productionBetaRelease", String.class), new InsteadHook(callFrame -> {
-                return getObject(authToken, callFrame);
-            }));
-        }
+        Field fAuthStateCache = StoreAuthentication.class.getDeclaredField("authStateCache");
+        fAuthStateCache.setAccessible(true);
+
+        Method mCacheAuthState = AuthStateCache.class.getDeclaredMethod("cacheAuthState", AuthState.class);
+        mCacheAuthState.setAccessible(true);
+
+        try {patcher.patch(StoreAuthentication.class.getDeclaredMethod("handleAuthState$app_productionGoogleRelease", AuthState.class), new InsteadHook(callFrame -> {
+            var storeAuth = (StoreAuthentication) callFrame.thisObject;
+            var authState = (AuthState) callFrame.args[0];
+ 
+            try {
+                fAuthState.set(storeAuth, authState);
+                var authStateCache = (AuthStateCache) fAuthStateCache.get(storeAuth);
+                mCacheAuthState.invoke(authStateCache, authState);
+            } catch (Exception e) {
+                logger.error(e);
+            }
+
+            if (authState == null&& !settings.getBool("persistSetting", true)) {
+                Persister.Companion.reset();
+                SharedPreferences.Editor editorEdit = storeAuth.getPrefs().edit();
+                m.checkNotNullExpressionValue(editorEdit, "editor");
+                editorEdit.clear();
+                editorEdit.apply();
+            }
+
+            return null;
+        })); } catch (NoSuchMethodException e) {};
 
         patcher.patch(StoreEmoji.class.getDeclaredMethod("handlePreLogout"), InsteadHook.DO_NOTHING);
 
         patcher.patch(StoreStickers.class.getDeclaredMethod("handlePreLogout"), InsteadHook.DO_NOTHING);
 
         patcher.patch(StoreNux.class.getDeclaredMethod("setFirstOpen", boolean.class), InsteadHook.DO_NOTHING);
-    }
-
-    @Nullable
-    private Object getObject(Field authToken, de.robv.android.xposed.XC_MethodHook.MethodHookParam callFrame) {
-        var storeAuth = (StoreAuthentication) callFrame.thisObject;
-        var str = (String) callFrame.args[0];
-
-        try {
-            authToken.set(callFrame.thisObject, str);
-        } catch (IllegalAccessException e) {
-            logger.error(e);
-        }
-
-        storeAuth.getPrefs().edit().putString("STORE_AUTHED_TOKEN", str).apply();
-        if (str == null && !settings.getBool("persistSetting", true)) {
-            Persister.Companion.reset();
-            SharedPreferences.Editor edit = storeAuth.getPrefs().edit();
-            m.checkExpressionValueIsNotNull(edit, "editor");
-            edit.clear();
-            edit.apply();
-        }
-
-        return null;
     }
 
     @Override
